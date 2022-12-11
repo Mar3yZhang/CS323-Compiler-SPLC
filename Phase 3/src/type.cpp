257170f4e69cdc6d0e96ebff9dbe0d6cf209f816
Marsy <87608363+MarsyCantStopCoding@users.noreply.github.com>
2022-12-11 22:16:04 +0800

    feat: start phase 3

---
+++ Phase 3/src/type.cpp
@@ -0,0 +1,49 @@
+#include "type.hpp"
+
+Type *_int_ = new Type("", CATEGORY::PRIMITIVE, PRIM::INT);
+Type *_float_ = new Type("", CATEGORY::PRIMITIVE, PRIM::FLOAT);
+Type *_char_ = new Type("", CATEGORY::PRIMITIVE, PRIM::CHAR);
+
+Type::Type(string name, CATEGORY category, PRIM primitive)
+{
+    this->name = name;
+    this->category = category;
+    this->foo.primitive = primitive;
+}
+Type::Type(string name, CATEGORY category, Array *array)
+{
+    this->name = name;
+    this->category = category;
+    this->foo.array = array;
+}
+Type::Type(string name, CATEGORY category, FieldList *structure)
+{
+    this->name = name;
+    this->category = category;
+    this->foo.structure = structure;
+}
+Type::Type(string name, CATEGORY category, FieldList *param, Type *returnType)
+{
+    this->name = name;
+    this->category = category;
+    this->foo.param = param;
+    this->returnType = returnType;
+}
+Type *Type::getPrimitiveINT()
+{
+    return _int_;
+}
+Type *Type::getPrimitiveCHAR()
+{
+    return _float_;
+}
+Type *Type::getPrimitiveFLOAT()
+{
+    return _char_;
+}
+
+Array::Array(int size, Type *base)
+    : size(size), base(base){};
+
+FieldList::FieldList(string name, Type *type)
+    : name(name), type(type){};
