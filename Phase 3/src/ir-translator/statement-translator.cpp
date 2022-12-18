e3ce16f5145a79f769c1138a319bcd604fa9542d
Marsy <87608363+MarsyCantStopCoding@users.noreply.github.com>
2022-12-18 21:01:42 +0800

    refactor: phase3 structure

---
+++ Phase 3/src/ir-translator/statement-translator.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Marsy on 2022/12/18.
+//
--- Phase 3/src/syntax.tab.c
+++ Phase 3/src/syntax.tab.c
@@ -2369,35 +2369,3 @@ void yyerror(const char *s){
     // }
 	// printf("ERROR: %s at symbol '%s' on line %d\n", s, yytext, yylineno);
 }
-//
-//
-//int main(int argc, char **argv) {
-//    if (argc <= 1) {
-//        printf("no input path");
-//        return 1;
-//    } else if (argc > 2) {
-//        printf("too much input path");
-//        return 1;
-//    } else {
-//        FILE *f = fopen(argv[1], "r");
-//        if (!f) {
-//            printf("error of path %s", argv[1]);
-//            return 1;
-//        }
-//        // printf("\nTokens: \n");
-//        yyrestart(f);
-//        if (!yyparse() && (!type_A_error) && (!type_B_error)) {
-//            //printf("\nParsing complete\n");
-//            //printf("\n\nAbstract Syntex Tree: \n");
-//
-//            // 是否选择打印语法分析树：
-//            //Node::print(ast_root,0);
-//            // 是否打印语义分析符号表
-//            // print_map_keys();
-//        } else {
-//            // printf("\nParsing failed\n");
-//        }
-//
-//    }
-//    return 0;
-//}
